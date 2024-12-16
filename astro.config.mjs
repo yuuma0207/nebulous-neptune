c160d6a6747bde92b5fe65840c75916e7422e969
yuuma <yuuma0207@gmail.com>
2024-12-16 23:00:39 +0900

    revised

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://yuuma0207.github.io',
   base: '/my-repo',
 })
