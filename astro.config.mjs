a0c88b00111f55192a758aedb621f4ddc9a1bba4
yuuma <yuuma0207@gmail.com>
2024-12-17 22:59:36 +0900

    revised: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yuuma0207.github.io',
-  base: '/my-repo',
+  base: '/',
 })
