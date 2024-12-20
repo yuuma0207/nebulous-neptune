e29591740aef652f5c96843801f6967c0f5b982a
yuuma <yuuma0207@gmail.com>
2024-12-20 09:04:21 +0900

    install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://yuuma0207.netlify.app"
+  site: "https://yuuma0207.netlify.app",
+  integrations: [preact()]
 });
