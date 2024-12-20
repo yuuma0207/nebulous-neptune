4f35547849aa6d62854ffa022caf1a03b7cd0315
yuuma <yuuma0207@gmail.com>
2024-12-20 10:52:09 +0900

    revised: pagerouting

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  return rss({
-    title: 'yuuma | ブログ',
-    description: 'Astroを学ぶ旅',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>ja-jp</language>`,
-  });
+    const posts = await getCollection('blogs');
+    return rss({
+        title: 'yuuma | ブログ',
+        description: 'Astroを学ぶ旅',
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `posts/${post.id}/`,
+        })),
+        customData: `<language>ja-jp</language>`,
+    });
 }
